bb0ded0f10d8e1966001bc4c1205576428b7e0ca
anamariakalabis <anamariabrazdau@yahoo.com>
2024-01-28 13:31:40 +0100

    new update

--- src/App.vue
+++ src/App.vue
@@ -1,69 +1,62 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
   <header>
-    
-   <nav>
-        <ul>
-            <li><a href="#about1">Présentation</a></li>
-            <li><a href="#creation">Créations</a></li>
-            <li><a href="#contact">Contact</a></li>
-        </ul>
-        <div id="backToTop" onclick="scrollToTop()">
-        
-        <img src="../assets/UP.png" alt="">
-    </div>
+    <nav>
+      <ul>
+        <li><a href="#about1">Présentation</a></li>
+        <li><a href="#creation">Créations</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+      <div id="backToTop" onclick="scrollToTop()">
+        <img src="../assets/UP.png" alt="" />
+      </div>
     </nav>
-   
-
   </header>
 
   <RouterView />
- 
 </template>
 
 <style scoped>
 nav {
-    background-color: black;
-    color: white;
-    padding: 10px;
+  background-color: black;
+  color: white;
+  padding: 10px;
 }
 
 nav ul {
-    list-style: none;
-    margin: 0;
-    padding: 0;
-    text-align: center;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  text-align: center;
 }
 
 nav ul li {
-    display: inline;
-    margin-right: 20px;
-
+  display: inline;
+  margin-right: 20px;
 }
 
 nav a {
-    text-decoration: none;
-    color: white;
-    font-weight: bold;
+  text-decoration: none;
+  color: white;
+  font-weight: bold;
 }
 
 section {
-    padding: 20px;
+  padding: 20px;
 }
 #backToTop {
-    position: fixed;
-    bottom: 20px;
-    right: 20px;
-    cursor: pointer;
-    display: none;
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+  cursor: pointer;
+  display: none;
 }
 
 #backToTop img {
-    width: 50px; /* Ajustez la taille de votre logo ou photo */
-    height: auto;
+  width: 50px; /* Ajustez la taille de votre logo ou photo */
+  height: auto;
 }
-
 </style>
